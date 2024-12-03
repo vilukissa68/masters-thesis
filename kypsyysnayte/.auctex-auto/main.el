1245071fe53405a49ae019bd2a82f1de067f0bc6
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-12-03 16:38:15 +0200

    PdfA compliance

--- kypsyysnayte/.auctex-auto/main.el
+++ kypsyysnayte/.auctex-auto/main.el
@@ -8,15 +8,15 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("lastpage" "") ("babel" "finnish") ("biblatex" "backend=biber" "style=numeric-comp" "citestyle=numeric-comp" "autocite=footnote" "maxbibnames=5") ("csquotes" "") ("booktabs" "") ("adjustbox" "") ("subcaption" "") ("caption" "") ("svg" "") ("acronym" "withpage") ("tikz" "") ("xcolor" "") ("amsfonts" "") ("listings" "") ("listings-rust" "") ("graphicx" "") ("float" "") ("multirow" "") ("calc" "")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "tunithesis"
@@ -47,16 +47,13 @@
     "checkmark"
     "scalecheck")
    (LaTeX-add-labels
-    "ch:introduction"
-    "fig:architecture")
+    "ch:introduction")
    (LaTeX-add-bibliographies
     "../thesis/thesis_refs"
     "../thesis/zotero")
    (LaTeX-add-listings-lstdefinestyles
     "customc"
     "customasm")
-   (LaTeX-add-acronyms
-    "VWW")
    (LaTeX-add-xcolor-definecolors
     "tunipurple"))
  :latex)
